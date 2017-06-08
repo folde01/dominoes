e6d775ef87d48db152f00f2bc9bc2bebb8d6e9f1
J O <jesseoldershaw@yahoo.co.uk>
2017-06-08 22:52:54 +0100

    first commit

---
+++ dominoes/players/DominoPlayer1.scala
@@ -0,0 +1,14 @@
+package dominoes.players
+
+class DominoPlayer1 extends DominoPlayer { 
+  def bonesInHand(): Array[dominoes.Bone] = ???
+  def draw(x$1: dominoes.BoneYard): Unit = ???
+  def getName(): String = ???
+  def getPoints(): Int = ???
+  def makePlay(x$1: dominoes.Table): dominoes.Play = ???
+  def newRound(): Unit = ???
+  def numInHand(): Int = ???
+  def setName(x$1: String): Unit = ???
+  def setPoints(x$1: Int): Unit = ???
+  def takeBack(x$1: dominoes.Bone): Unit = ???
+}
