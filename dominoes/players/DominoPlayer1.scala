5b405ea98ecc4fd387aef5b48e640a70dfb0cdd9
J O <jesseoldershaw@yahoo.co.uk>
2017-06-11 19:22:14 +0100

    trying to make a computer player that picks a random tile so we can play 2 against each other

--- dominoes/players/DominoPlayer1.scala
+++ dominoes/players/DominoPlayer1.scala
@@ -6,21 +6,71 @@ import org.scalacheck.Prop.forAll
 
 class DominoPlayer1 extends DominoPlayer { 
 
+  var name: String = "not set" 
+  var points = 0 
   var bones = Array[Bone]()
-  def getBones = bones
+  //def getBones = bones
+  //var playChooser: PlayChooser = null
 
-  def bonesInHand(): Array[dominoes.Bone] = ???
-  def draw(yard: dominoes.BoneYard): Unit = { 
+  def bonesInHand(): Array[Bone] = bones 
+
+  def draw(yard: BoneYard): Unit = { 
     val bone = yard.draw()
     if (bone != null) 
       bones = bones :+ bone 
   }
-  def getName(): String = ???
-  def getPoints(): Int = ???
-  def makePlay(x$1: dominoes.Table): dominoes.Play = ???
-  def newRound(): Unit = ???
+
+  def getName(): String = name 
+
+  def getPoints(): Int = points
+
+  def makePlay(table: Table): Play = {
+    val play = choosePlay
+    val bone = play.bone
+    val playString = s"${bone.left}:${bone.right} at ${play.end}"
+
+    try { 
+      //output(s"Attempting: $playString") 
+      table.play(play)
+      //output(s"Accepted: $playString") 
+    } catch { 
+      case e: InvalidPlayException => { 
+        takeBack(bone)
+        makePlay(table)
+      }
+    }
+  }
+
+  def newRound(): Unit = bones = Array[Bone]()
+
   def numInHand(): Int = bones.length 
-  def setName(x$1: String): Unit = ???
-  def setPoints(x$1: Int): Unit = ???
-  def takeBack(x$1: dominoes.Bone): Unit = ???
+
+  def setName(n: String): Unit = name = n 
+
+  def setPoints(newScore: Int): Unit = points = newScore 
+
+  def takeBack(bone: Bone): Unit = { 
+    val s = "Rejected: ${bone.left}:${bone.right}"
+    output(s)
+  }
+
+  def choosePlay: Play = { 
+    val r = scala.util.Random
+    val bone = bones(r.nextInt(bones.size)) 
+
+    if (r.nextInt(2) == 1) 
+      bone.flip 
+
+    val end = { 
+      if (r.nextInt(2) == 1) Play.RIGHT
+      else Play.LEFT
+    }
+
+    new Play(bone, end)
+  }
+
+  //def setPlayChooser(pc: PlayChooser): Unit = playChooser = pc 
+
+  def output(s: String) = println(s)
+
 }
--- dominoes/players/DominoPlayerTest.scala
+++ dominoes/players/DominoPlayerTest.scala
@@ -9,6 +9,31 @@ object DominoPlayerSpecification extends Properties("DominoPlayer") {
   val boneTop = 6
   val maxDraws = 28 
   val validNumDraws = Gen.choose(0,maxDraws)
+
+
+  property("getPoints == setPoints") = forAll { (newScore: Int) => 
+    val p: Player = new Player 
+    p.setPoints(newScore)
+    p.getPoints == newScore
+  }
+
+  property("getName == setName") = forAll { (name: String) => 
+    val p: Player = new Player 
+    p.setName(name)
+    p.getName == name
+  }
+  
+  property("bonesInHand.size == numInHand") = forAll(validNumDraws) { (n: Int) => 
+    // validNumDraws is producing negative numbers for some reason so 
+    // use a condition to make it positive until we work out why:
+    (n >= 1) ==> { 
+      val player: Player = new Player 
+      val yard: BoneYard = new BoneYard(boneTop)
+      for (j <- 1 to n) 
+        player.draw(yard)
+      player.bonesInHand.size == n 
+    }
+  }
   
   property("numInHand == number of draws") = forAll(validNumDraws) { (n: Int) => 
     // validNumDraws is producing negative numbers for some reason so 
@@ -16,16 +41,16 @@ object DominoPlayerSpecification extends Properties("DominoPlayer") {
     (n >= 1) ==> { 
       val player: Player = new Player 
       val yard: BoneYard = new BoneYard(boneTop)
-      println(s"############### n: $n size: ${yard.size}")
-      println(s"numInHand: " + player.numInHand)
+      //println(s"############### n: $n size: ${yard.size}")
+      //println(s"numInHand: " + player.numInHand)
 
       for (j <- 1 to n) { 
         player.draw(yard)
-        for (i <- 0 until player.getBones.size) { 
-           val b = player.getBones(i); print((b.left, b.right) + " ") }
-        println(s"numInHand: " + player.numInHand)
+        //for (i <- 0 until player.getBones.size)
+        //  val b = player.getBones(i); print((b.left, b.right) + " ")
+        //println(s"numInHand: " + player.numInHand)
       }
-      println(s"FINAL numInHand: " + player.numInHand)
+      //println(s"FINAL numInHand: " + player.numInHand)
       player.numInHand == n 
     }
   }
