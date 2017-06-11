1ee56b76906d7b0d7af77b4079828f367ccf115b
J O <jesseoldershaw@yahoo.co.uk>
2017-06-11 13:52:15 +0100

    first passing test: DominoPlayer.numInHand == number of draws ... but why is our Gen producing negative numbers?

--- dominoes/players/DominoPlayer1.scala
+++ dominoes/players/DominoPlayer1.scala
@@ -1,13 +1,25 @@
 package dominoes.players
 
+import dominoes._
+import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+
 class DominoPlayer1 extends DominoPlayer { 
+
+  var bones = Array[Bone]()
+  def getBones = bones
+
   def bonesInHand(): Array[dominoes.Bone] = ???
-  def draw(x$1: dominoes.BoneYard): Unit = ???
+  def draw(yard: dominoes.BoneYard): Unit = { 
+    val bone = yard.draw()
+    if (bone != null) 
+      bones = bones :+ bone 
+  }
   def getName(): String = ???
   def getPoints(): Int = ???
   def makePlay(x$1: dominoes.Table): dominoes.Play = ???
   def newRound(): Unit = ???
-  def numInHand(): Int = ???
+  def numInHand(): Int = bones.length 
   def setName(x$1: String): Unit = ???
   def setPoints(x$1: Int): Unit = ???
   def takeBack(x$1: dominoes.Bone): Unit = ???
--- dominoes/players/DominoPlayerTest.scala
+++ dominoes/players/DominoPlayerTest.scala
@@ -1,16 +1,32 @@
 package dominoes
 
-import org.scalacheck.Properties
-import org.scalacheck.Prop.forAll
+import org.scalacheck.{Properties,Gen}
+import org.scalacheck.Prop.{forAll, BooleanOperators}
 
 object DominoPlayerSpecification extends Properties("DominoPlayer") {
 
   type Player = players.DominoPlayer1 
+  val boneTop = 6
+  val maxDraws = 28 
+  val validNumDraws = Gen.choose(0,maxDraws)
   
-  property("bonesInHand") = forAll { (n: Int) => 
-    //val player: Player = new Player 
-    val table: Table = new Table 
-    n == n
-  }
+  property("numInHand == number of draws") = forAll(validNumDraws) { (n: Int) => 
+    // validNumDraws is producing negative numbers for some reason so 
+    // use a condition to make it positive until we work out why:
+    (n >= 1) ==> { 
+      val player: Player = new Player 
+      val yard: BoneYard = new BoneYard(boneTop)
+      println(s"############### n: $n size: ${yard.size}")
+      println(s"numInHand: " + player.numInHand)
 
+      for (j <- 1 to n) { 
+        player.draw(yard)
+        for (i <- 0 until player.getBones.size) { 
+           val b = player.getBones(i); print((b.left, b.right) + " ") }
+        println(s"numInHand: " + player.numInHand)
+      }
+      println(s"FINAL numInHand: " + player.numInHand)
+      player.numInHand == n 
+    }
+  }
 }
