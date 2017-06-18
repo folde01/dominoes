397e5fcbe42e050fd99b109f5ccb70392b9ffaf0
J O <jesseoldershaw@yahoo.co.uk>
2017-06-18 01:01:35 +0100

    working on cubby issues around passing one's turn

---
+++ dominoes/DominoUI3.scala
@@ -0,0 +1,56 @@
+package dominoes
+
+import dominoes._
+import dominoes.players._
+
+class DominoUI3 extends DominoUI { 
+
+  var cubby: CubbyHole = null
+
+  def setCubby(c: CubbyHole) = cubby = c
+
+  def display(players: Array[DominoPlayer],table: Table, yard: BoneYard): Unit = { 
+    //println("---")
+    displayTable(table)
+    displayBoneYard(yard)
+    //displayHands(players)
+    //println("---")
+    cubby.put(table.layout) 
+  }
+
+  def displayInvalidPlay(x$1: dominoes.players.DominoPlayer): Unit = println("displayInvalidPlay")
+  def displayRoundWinner(x$1: dominoes.players.DominoPlayer): Unit = println("displayRoundWinner") 
+  def displayHands(players: Array[DominoPlayer]): Unit = { 
+    players.foreach { (player) =>
+      val hand = player.bonesInHand.map(boneString).mkString(" ")
+      println("\n  " + player.getName + "'s hand: " + hand)
+    }
+  }
+
+  def displayTable(table: Table): Unit = { 
+    val layoutString = 
+      if (table.layout.nonEmpty) { 
+         table.layout.map(boneString).mkString(" ") 
+      } else ""
+
+    displayLayout(layoutString)
+  }
+
+  def displayLayout(layout: String): Unit = { 
+    println("\n  * * * *")
+    println("  * * * *")
+    println("  Layout:")
+    println("  " + layout)
+    println("  * * * *")
+    println("  * * * *")
+  }
+
+  def displayBoneYard(yard: BoneYard): Unit = 
+    println("\n  Boneyard size: " + yard.size) 
+
+
+  def boneString(bone: Bone): String = 
+    s"${bone.left.toString}:${bone.right.toString}"
+
+}
+
---
+++ dominoes/players/DominoPlayer3.scala
@@ -0,0 +1,251 @@
+package dominoes.players
+
+import dominoes._
+
+class DominoPlayer3(cubby: CubbyHole, human: Boolean) extends DominoPlayer { 
+
+  val id = scala.util.Random.nextInt(100000)
+
+  var counter = 0  
+  val debug = false
+  //val debug = true
+
+  // from CubbyHole
+  var cubbyLayout = Array[Bone]()
+  
+  var shouldLog = true
+  var name: String = "not set" 
+  var points = 0 
+  var bones = Array[Bone]()
+  //def getBones = bones
+  //var playChooser: PlayChooser = null
+
+  def bonesInHand(): Array[Bone] = { 
+    log("bonesInHand: " + bonesToString(bones))
+    bones 
+  }
+
+  def draw(yard: BoneYard): Unit = { 
+    val bone = yard.draw()
+    println("\n  D R A W . . . " + getName + " drew " + boneString(bone))
+    if (bone != null) 
+      bones = bones :+ bone 
+    log("draw: " + bone.left + ":" + bone.right) 
+  }
+
+  def getName(): String = name 
+
+  def getPoints(): Int = { 
+    log("getPoints: " + points)
+    points
+  }
+
+  def makePlay(table: Table): Play = {
+    log("makePlay")
+    displayHand
+
+    if (human) { 
+      val boneOption = requestBone
+
+      boneOption match { 
+        case None => 
+          setCubbyLayout
+          throw new CantPlayException("No bone received from requestBone.")
+        case _ => { 
+          val bone = boneOption.get
+          val end = requestEnd
+          val play = new Play(bone, end)
+          val left = bone.left
+          val right = bone.right
+          val playString = left + ":" + right + " on the " + 
+            { if (end == 0) "left" else "right" }
+          println("\n  <<<<<< " + getName + " playing " + playString + " >>>>>>")
+          val newbones = bones.filter(b => (!b.equals(bone)))
+          log("newbones: " + bonesToString(newbones))
+          bones = newbones
+          play
+        }
+      }
+    } else { 
+      val playOption = choosePlay
+      
+      playOption match { 
+        case None => 
+          setCubbyLayout
+          throw new CantPlayException("No play received from choosePlay.")
+        case Some(play) => { 
+          val left = play.bone.left
+          val right = play.bone.right
+          val playString = left + ":" + right + " on the " + 
+            { if (play.end == 0) "left" else "right" }
+          println("\n  <<<<<< " + getName + " playing " + playString + " >>>>>>")
+          val newbones = bones.filter(b => (!b.equals(play.bone)))
+          log("newbones: " + bonesToString(newbones))
+          bones = newbones
+          play
+        }
+      }
+    }
+  }
+
+  def getCubbyLayout = cubbyLayout = cubby.get.asInstanceOf[Array[Bone]]
+
+  def setCubbyLayout = cubby.put(cubbyLayout)
+
+  def canPlayFromHand = { 
+    log("canPlayFromHand")
+    //val layout = cubby.get.asInstanceOf[Array[Bone]]
+    val layout = cubbyLayout
+    log("Layout from cubby: " + bonesToString(layout))
+
+    // check each in hand to see if it has left or right
+    val leftEnd = layout.head.left
+    val rightEnd = layout.last.right
+    def hasInt(b: Bone, n: Int) = b.left == n || b.right == n
+    bones.exists(hasInt(_,leftEnd)) || bones.exists(hasInt(_,rightEnd))
+  }
+
+  def choosePlay: Option[Play] = { 
+    log("choosePlay")
+    val layout = cubby.get.asInstanceOf[Array[Bone]]
+    log("Layout from cubby: " + bonesToString(layout))
+
+    // check each in hand to see if it has left or right
+    val leftEnd = layout.head.left
+    val rightEnd = layout.last.right
+    log("leftEnd: " + leftEnd + ", rightEnd: " + rightEnd) 
+
+    def hasInt(b: Bone, n: Int) = b.left == n || b.right == n
+
+    val boneOptionMatchingLeftEnd = bones.find(hasInt(_,leftEnd))
+    val boneOptionMatchingRightEnd = bones.find(hasInt(_,rightEnd))
+
+    boneOptionMatchingLeftEnd match { 
+      case Some(bone) => Some(new Play(bone, Play.LEFT))
+      case None => { 
+        boneOptionMatchingRightEnd match { 
+          case Some(bn) => Some(new Play(bn, Play.RIGHT))
+          case None => None
+        }
+      }
+    }
+  }
+
+  def requestBone: Option[Bone] = { 
+    log("requestBone")
+    // todo: how do we end current game?
+
+    val cantPlay = "cp"
+
+    print(s"\nEnter bone to play (eg '0:1') or '$cantPlay' if you can't play: ")
+    val entered = scala.io.StdIn.readLine().trim
+
+    if (entered == cantPlay) { 
+      if (canPlayFromHand) { 
+        println("You have a bone you can play. Try again.")
+        requestBone
+      } else { 
+        None
+      }
+    } else { 
+      if (isValidBoneString(entered)) { 
+        val boneOption = makeBoneOptionFromString(entered)
+        val bone = boneOption.get
+        println("You entered " + bone.left + ":" + bone.right) 
+        if (handHasBone(bone)) { 
+          boneOption
+        } else { 
+          println("That's not in your hand. Try again.")
+          requestBone
+        }
+      } else { 
+        println("That's not a bone. Try again.")
+        requestBone
+      }
+    }
+  }
+
+
+  def requestEnd: Int = { 
+    log("requestEnd")
+    println("Enter the end to play on (0 for left, 1 for right): ")
+    val entered = scala.io.StdIn.readLine().trim
+
+    entered match { 
+      case "0" => Play.LEFT 
+      case "1" => Play.RIGHT 
+      case _ => { 
+        println("Not a valid end. Try again.")
+        requestEnd 
+      }
+    }
+  }
+     
+  def isValidBoneString(s: String): Boolean = { 
+    log("isValidBoneString")
+    val rgx = "^\\s*[0-9]+:[0-9]+\\s*$"
+    s.matches(rgx)
+  }
+
+  def makeBoneOptionFromString(s: String): Option[Bone] = { 
+    log("makeBoneFromString")
+    if (isValidBoneString(s)) 
+      Some(new Bone(s.split(":")(0).toInt, s.split(":")(1).toInt))
+    else
+      None
+  }
+
+  def handHasBone(b: Bone) = bones.exists(_.equals(b))
+
+  def displayHand: Unit = { 
+    log("displayHand")
+    println("\n#####   " + getName + "'s turn" + "   #####")
+    if (numInHand == 0) { 
+      println("\nYour hand is empty.")
+    }
+    else { 
+      println("\nYour hand: " + bonesToString(bones))
+    }
+  }
+
+  def boneString(bone: Bone): String =
+    s"${bone.left.toString}:${bone.right.toString}"
+
+  def bonesToString(bones: Seq[Bone]): String = { 
+      bones.map(boneString).mkString(" ")
+  }
+
+
+  def newRound(): Unit = { 
+    log("new round")
+    bones = Array[Bone]()
+  }
+
+  def numInHand(): Int = { 
+    log("numInHand: " + bones.length)
+    bones.length 
+  }
+
+  def setName(n: String): Unit = name = n 
+
+  def setPoints(newScore: Int): Unit = { 
+    log("setPoints:" + newScore)
+    points = newScore 
+  }
+
+  def takeBack(bone: Bone): Unit = { 
+    log("takeBack: " + bone.left + ":" + bone.right)
+    val newbones = bones :+ bone
+    bones = newbones
+    // todo: put back so hand is in same order as before and not flipped
+  }
+
+  def log(s: String) = { 
+    if (debug) { 
+      if (shouldLog)
+        println(id + ":" + counter + ": " + s)
+      counter += 1
+    }
+  }
+
+}
